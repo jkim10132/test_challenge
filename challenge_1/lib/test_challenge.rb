5ca1eaf9d45790ad9747f08594027319d2679024
Bryan Lim <bryan.lim@procore.com>
2018-07-27 12:43:28 -0700

    Justin

--- challenge_1/lib/test_challenge.rb
+++ challenge_1/lib/test_challenge.rb
@@ -1,5 +1,5 @@
 def print_name
   #TODO: Print Name
   print "name"
-  #Justin, please accept me. Samar's gaze has dried out. He no longer loves me.
+  #Justin, please accept me. Samar's gaze has dried out. He no longer loves me!
 end
